8d9901c457252ef6346be11b0a052d4d9e11ccc8
sablib <sablib.iak@gmail.com>
2016-01-16 19:11:32 +0800

    Bugfix.

--- ZZOperationExtension/Classes/ZZDelayOperation.m
+++ ZZOperationExtension/Classes/ZZDelayOperation.m
@@ -49,9 +49,7 @@
     }
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(interval * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        if (!self.isCancelled) {
-            [self finish];
-        }
+        [self finish];
     });
 }
 
